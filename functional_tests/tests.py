d82f5f6a58c2debacbbe22f6906084df71118cbd
sameerank <skunche@gmail.com>
2017-08-21 10:09:54 -0700

    Remove voodoo sleeps

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,54 +1,61 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
+
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 
-  def setUp(self):
-    self.browser = webdriver.Chrome(service_args=["--log-path=./chromedriver.log"])
+    def setUp(self):
+        self.browser = webdriver.Chrome(service_args=["--log-path=./chromedriver.log"])
 
-  def tearDown(self):
-    self.browser.quit()
+    def tearDown(self):
+        self.browser.quit()
 
-  def test_can_start_a_list_and_retrieve_it_later(self):
-    # Edith has heard about a cool new online to-do app. She goes to check out its homepage
-    self.browser.get(self.live_server_url)
+    def wait_for_row_in_list_tables(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
-    # She is invited to enter a to-do item straight away
-    self.assertIn('To-Do', self.browser.title)
-    header_text = self.browser.find_element_by_tag_name('h1').text
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes to check out its homepage
+        self.browser.get(self.live_server_url)
 
-    self.assertIn('To-Do', header_text)
+        # She is invited to enter a to-do item straight away
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
 
-    # She is invited to enter a to-do item straight away
-    inputbox = self.browser.find_element_by_id('id_new_item')
-    self.assertEqual(
-      inputbox.get_attribute('placeholder'),
-      'Enter a to-do item'
-    )
+        self.assertIn('To-Do', header_text)
 
-    # She types "Buy peacock feathers" into a text box
-    inputbox.send_keys('Buy peacock feathers')
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+          inputbox.get_attribute('placeholder'),
+          'Enter a to-do item'
+        )
 
-    inputbox.send_keys(Keys.ENTER)
-    time.sleep(1)
+        # She types "Buy peacock feathers" into a text box
+        inputbox.send_keys('Buy peacock feathers')
 
-    table = self.browser.find_element_by_id('id_list_table')
-    rows = table.find_elements_by_tag_name('tr')
-    self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_tables('1: Buy peacock feathers')
 
-    # There is still a text box for adding another item. She enters use peacock
-    # feathers to make a fly.
-    inputbox = self.browser.find_element_by_id('id_new_item')
-    inputbox.send_keys('Use peacock feathers to make a fly')
-    inputbox.send_keys(Keys.ENTER)
-    time.sleep(1)
+        # There is still a text box for adding another item. She enters use peacock
+        # feathers to make a fly.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_tables('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_tables('1: Buy peacock feathers')
 
-    # The page updates and displays both items in the table
-    table = self.browser.find_element_by_id('id_list_table')
-    rows = table.find_elements_by_tag_name('tr')
-    self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-    self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
-
-    self.fail('Finish the test!')
+        self.fail('Finish the test!')
