cdb1de1511691effecd54767d0f1e54c8389f56f
sameerank <skunche@gmail.com>
2017-08-26 19:21:02 -0700

    Add a lists url

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -75,14 +75,15 @@ class NewVisitorTest(LiveServerTestCase):
         ## We use a new browser session to make sure that no information
         ## of Edith's is coming through from cookies etc
         self.browser.quit()
-        self.browser = webdriver.Chrome(service_args=["--log-path=./chromedriver.log"])
+        self.browser = webdriver.Chrome()
+        self.browser.get(self.live_server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
 
         # Francis starts a new list by entering a new item.
         # He is less interesting than Edith ...
-        input = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_tables('1: Buy milk')
